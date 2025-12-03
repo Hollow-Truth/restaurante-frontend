c7bc4876499b47c27efb1f8cd03140a458fd1cb4
Hollow-Truth <HollowTruth@outlook.com>
2025-12-03 15:40:47 -0400

    Aumentar budget de compilación para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-      production: true,
-      apiUrl: 'https://backend-restaurante-wzip.onrender.com/api'
-    };
+  production: true,
+  apiUrl: 'https://backend-restaurante-wzip.onrender.com/api' 
+};
